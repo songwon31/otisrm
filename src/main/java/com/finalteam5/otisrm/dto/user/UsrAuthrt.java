48e5dd5d5c2bd162f3c0088c576def19432c5332
songwon31 <sws1719@naver.com>
2023-10-19 15:13:57 +0900

    수정

--- src/main/java/com/finalteam5/otisrm/dto/user/UsrAuthrt.java
+++ src/main/java/com/finalteam5/otisrm/dto/user/UsrAuthrt.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class UsrAuthrt {
-	String userAuthrtNo;		//사용자 권한 번호
-	String userAuthrtNm;		//사용자 권한 이름
+	String usrAuthrtNo;		//사용자 권한 번호
+	String usrAuthrtNm;		//사용자 권한 이름
 }
